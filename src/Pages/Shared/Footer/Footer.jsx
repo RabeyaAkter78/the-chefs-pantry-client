1289759944aa0c178e9b45f0d18bb05808ea9792
RabeyaAkter78 <rabeyariya251@gmail.com>
2023-05-02 16:31:54 +0600

    added footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { FaCopyright, FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { FaBeer } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            <p className='text-center'><small>all copyright reserved by chefs pantry</small></p>
-        </div>
+        <Container className='text-white bg-dark p-2 '>
+            <Container>
+                <Row>
+                    <Col xs={{ order: 'first' }}>
+                        <h5>Office Address</h5>
+                        <p>Uttara,Dhaka,Bangladesh</p>
+                    </Col>
+                    <Col xs >
+                        <h5>Useful Links</h5>
+                        <Link style={{ textDecoration: 'none' }} to="/home">Home</Link>
+                        <br />
+                        <Link style={{ textDecoration: 'none' }} to="/blog">Blogs</Link>
+                    </Col>
+                    <Col xs={{ order: 'last ' }}>
+                        <h5>Follow Us</h5>
+                        <div className='d-flex align-items-center gap-2 py-4'>
+                            <FaFacebook />
+                            <FaInstagram />
+                            <FaTwitter />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <p className='container text-center mt-4'><small><FaCopyright /> All Copyright Reserved By Chefs Pantry</small></p>
+        </Container>
     );
 };
 
