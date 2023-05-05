50d3d3379a877b446d4b0949ebdba86c4b9c2f7b
RabeyaAkter78 <rabeyariya251@gmail.com>
2023-05-05 22:49:05 +0600

    change error img

--- src/component/Error/Error.jsx
+++ src/component/Error/Error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
     return (
         <Container>
             <Card className="bg-dark text-white">
-                <Card.Img src="https://thumbs.dreamstime.com/b/page-not-found-error-power-out-50850182.jpg" alt="Card image" />
+                <Card.Img src="https://i.pinimg.com/originals/49/e5/8d/49e58d5922019b8ec4642a2e2b9291c2.png?fbclid=IwAR1Z_CEqK3j7k9ZElHhdGoVZaosU8Ip3yFe-Rlinn6UUXiFbSR4RGR54NWk" alt="Card image" />
                 <Card.ImgOverlay>
                     <Card.Title>404</Card.Title>
                     <Card.Text>
